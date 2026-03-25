2d5f77a0e746a2be223f4c86d8af5fd3621c3fc0
utkarsh raj <utkarshr571@gmail.com>
2026-03-25 14:36:59 +0530

    4th commit

--- quora_post/index.js
+++ quora_post/index.js
@@ -2,42 +2,49 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const { v4: uuidv4 } = require('uuid');                                             //to give unique id to each post
+
 const path = require('path');
 
 
-app.use(express.urlencoded({ extended: true }));             // url encoded data ko parse karne ke liye
+app.use(express.urlencoded({ extended: true }));                                   // url encoded data ko parse karne ke liye
 
-app.set("view engine", "ejs");                   // ejs ko set kiya
-app.set("views", path.join(__dirname, "views")); // views ko set kiya
+app.set("view engine", "ejs");                                                   // ejs ko set kiya
+app.set("views", path.join(__dirname, "views"));                                // views ko set kiya
 
-app.use(express.static(path.join(__dirname, "public")));          // css file ko serve karne ke liye
+app.use(express.static(path.join(__dirname, "public")));                        // css file ko serve karne ke liye
 
 let posts = [
     {
-        id: "1a",
+        id:uuidv4(),
         username: "utkarsh",
         content: "hello everyone"
     },
     {
-        id: "2a",
+        id:uuidv4(),
         username: "raj",
         content: "hello everyone"
     },
     {
-        id: "3a",
+        id:uuidv4(),
         username: "harsh",
         content: "hello everyone"
     },
     {
-        id: "4a",
+        id:uuidv4(),
         username: "shilpee",
         content: "hello everyone"
     }
 ]
 
+
+//index route
+
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 })
+
+//new route
 
 app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
@@ -46,11 +53,12 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
     // console.log(username,content);
-    posts.push({ username, content });
+    let id = uuidv4();
+    posts.push({ id,username, content });
     res.redirect("/posts");
 })
 
-
+//show route
 
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
@@ -62,6 +70,12 @@ app.get("/posts/:id", (req, res) => {
     
     res.render("show.ejs",{post});
 })
+
+
+// update route
+
+
+
 
 app.listen(port, () => {
     console.log("server is listening port 8080");
