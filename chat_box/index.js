bdd3b0a998c7ec733740ff82f1da089e4db44f4d
utkarsh raj <utkarshr571@gmail.com>
2026-03-30 17:51:17 +0530

    9th commit

---
+++ chat_box/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+// database connection
+
+const mongoose = require('mongoose');
+
+main()
+    .then(()=>{
+        console.log("database connected");
+    }).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/chat_box');
+}
+
+app.get("/",(req,res) =>{
+    res.send("root is working");
+})
+
+app.listen(8080, ()=>{
+    console.log("server is running on port 8080");
+})
